c37c3cd0e925f4d064c8e0113c6aabe067b6607b
csaper6 <csaper6@CompSci-36.local>
2016-10-05 13:02:48 -0700

    Initial commit

---
+++ app/src/main/java/com/example/csaper6/csaproject1_mini_games3dtictactoemrshorr2016/TicTacToe.java
@@ -0,0 +1,13 @@
+package com.example.csaper6.csaproject1_mini_games3dtictactoemrshorr2016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TicTacToe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tic_tac_toe);
+    }
+}
