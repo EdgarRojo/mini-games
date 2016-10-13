936c4bbd19854d265af1c5c275c7af359060470a
Phoebe <phoebekim99@gmail.com>
2016-10-13 14:00:37 -0700

    MathGame

--- app/src/main/java/com/example/csaper6/csaproject1_mini_games3dtictactoemrshorr2016/MathGame.java
+++ app/src/main/java/com/example/csaper6/csaproject1_mini_games3dtictactoemrshorr2016/MathGame.java
@@ -2,60 +2,165 @@ package com.example.csaper6.csaproject1_mini_games3dtictactoemrshorr2016;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MathGame extends AppCompatActivity {
+public class MathGame extends Numbers {
     private double a;
     private double b;
-    private double c;
-    private double d;
-    private double e;
-    private double f;
     private double fButtonValue;
     private double sButtonValue;
     private double tButtonValue;
     private Button fButton;
     private Button sButton;
     private Button tButton;
-    private int fsign;
-    private int ssign;
-    private int tsign;
-    private String fsymbol;
-    private String ssymbol;
-    private String tsymbol;
+    private String symbol;
+    private int points;
+    private TextView point;
+    private Button next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_math_game);
 
+        points = 0;
+
         fButton = (Button) findViewById(R.id.fButton);
         sButton = (Button) findViewById(R.id.sButton);
         tButton = (Button) findViewById(R.id.tButton);
+        point = (TextView) findViewById(R.id.point);
+        next = (Button) findViewById(R.id.next);
 
+        point.setText(Integer.toString(points));
 
-        a = (int) (Math.random() * 1000);
-        b = (int) (Math.random() * 1000);
-        c = (int) (Math.random() * 1000);
-        d = (int) (Numbers.random() * 1000);
-        e = (int) (Numbers.random() * 1000);
-        f = (int) (Numbers.random() * 1000);
-        fsign = (int) (Numbers.random() * 4);
-        ssign = (int) (Numbers.random() * 4);
-        tsign = (int) (Numbers.random() * 4);
+        update();
 
-        if (fsign == 0){
-            fsymbol = "+";
-            fButtonValue = a + b;
-        }
-        else if (fsign == 1){
-            fsymbol = "-";
-            fButtonValue = a - b;
-        }
-        else{
-            fsymbol = "*";
-        }
+        fButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkAnswer(fButtonValue)){
+                    points ++;
+                }
+                else{
+                    points --;
+                }
+                point.setText(Integer.toString(points));
+                disableButtons();
+            }
+        });
 
+        sButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkAnswer(sButtonValue)){
+                    points++;
+                }
+                else{
+                    points--;
+                }
+                point.setText(Integer.toString(points));
+                disableButtons();
+            }
+        });
+
+        tButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkAnswer(tButtonValue)){
+                    points++;
+                }
+                else{
+                    points--;
+                }
+                point.setText(Integer.toString(points));
+                disableButtons();
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                enableButtons();
+                update();
+            }
+        });
 
     }
+
+    private void enableButtons() {
+        fButton.setEnabled(true);
+        sButton.setEnabled(true);
+        tButton.setEnabled(true);
+    }
+
+    private void disableButtons() {
+        fButton.setEnabled(false);
+        sButton.setEnabled(false);
+        tButton.setEnabled(false);
+    }
+
+    private boolean checkAnswer(double d) {
+        if (d >= fButtonValue && d>=sButtonValue && d>=tButtonValue){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    private void update() {
+        a = generateValues() * 1.0;
+        b = generateValues() * 1.0;
+        symbol = generateSign();
+        fButton.setText(Double.toString(a) + " " + symbol + " " + Double.toString(b));
+        if (symbol.equals("+")){
+            fButtonValue = a + b;
+        }
+        else if (symbol.equals("-")){
+            fButtonValue = a - b;
+        }
+        else if (symbol.equals("*")){
+            fButtonValue = a * b;
+        }
+        else{
+            fButtonValue = a/b;
+        }
+        a = generateValues() * 1.0;
+        b = generateValues() * 1.0;
+        symbol = generateSign();
+        sButton.setText(Double.toString(a) + " " + symbol + " " + Double.toString(b));
+        if (symbol.equals("+")){
+            sButtonValue = a + b;
+        }
+        else if (symbol.equals("-")){
+            sButtonValue = a - b;
+        }
+        else if (symbol.equals("*")){
+            sButtonValue = a * b;
+        }
+        else{
+            sButtonValue = a/b;
+        }
+        a = generateValues() * 1.0;
+        b = generateValues() * 1.0;
+        symbol = generateSign();
+        tButton.setText(Double.toString(a) + " " + symbol + " " + Double.toString(b));
+        if (symbol.equals("+")){
+            tButtonValue = a + b;
+        }
+        else if (symbol.equals("-")){
+            tButtonValue = a - b;
+        }
+        else if (symbol.equals("*")){
+            tButtonValue = a * b;
+        }
+        else{
+            tButtonValue = a/b;
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/csaper6/csaproject1_mini_games3dtictactoemrshorr2016/Numbers.java
+++ app/src/main/java/com/example/csaper6/csaproject1_mini_games3dtictactoemrshorr2016/Numbers.java
@@ -1,32 +1,34 @@
 package com.example.csaper6.csaproject1_mini_games3dtictactoemrshorr2016;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by csaper6 on 10/11/16.
  */
-public class Numbers {
+public class Numbers extends AppCompatActivity {
     private int a;
     private int b;
-    private int c;
 
-    public String generateProblem(){
+
+    public int generateValues(){
         a = (int) (Math.random() * 1000);
-        b = (int) (Math.random() * 1000);
-        c = (int) (Math.random() * 4);
-        if (c == 0){
-            return Integer.toString(a) + "+" + Integer.toString(b);
+        return a;
+    }
+
+    public String generateSign(){
+        b = (int) (Math.random() * 4);
+        if (b == 0){
+            return "+";
         }
-        else if (c == 1){
-            return Integer.toString(a) + "-" + Integer.toString(b);
+        else if (b == 1){
+            return "-";
         }
-        else if (c == 2){
-            return Integer.toString(a) + "*" + Integer.toString(b);
+        else if (b == 2){
+            return "*";
         }
         else{
-            return Integer.toString(a) + "/" + Integer.toString(b);
+            return "/";
         }
     }
-
-    public int generateValue(){
-
-    }
 }
+
